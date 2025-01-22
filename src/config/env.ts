85d10d1e929ec75f9158c999e6b1254a3063f981
Daniel Santamaria <santamaria.daniel316@gmail.com>
2025-01-22 15:14:59 -0800

    Add Logger and env validation

---
+++ src/config/env.ts
@@ -0,0 +1,50 @@
+import "dotenv/config";
+import { z } from "zod";
+import { Logger } from "@/utils/logger";
+
+const logger = new Logger("Config:Env");
+
+// Schema for environment variables
+const envSchema = z.object({
+  CLERK_SECRET_KEY: z.string(),
+  NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string(),
+  NEXT_PUBLIC_CLERK_SIGN_IN_FORCE_REDIRECT_URL: z.string(),
+  NEXT_PUBLIC_CLERK_SIGN_IN_FALLBACK_REDIRECT_URL: z.string(),
+  NEXT_PUBLIC_CLERK_SIGN_UP_FORCE_REDIRECT_UR: z.string(),
+  DATABASE_URL: z.string(),
+});
+
+// Function to validate environment variables
+const validateEnv = () => {
+  try {
+    logger.info("Validating environment variables");
+    const env = {
+      CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
+      NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY:
+        process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+      NEXT_PUBLIC_CLERK_SIGN_IN_FORCE_REDIRECT_URL:
+        process.env.NEXT_PUBLIC_CLERK_SIGN_IN_FORCE_REDIRECT_URL,
+      NEXT_PUBLIC_CLERK_SIGN_IN_FALLBACK_REDIRECT_URL:
+        process.env.NEXT_PUBLIC_CLERK_SIGN_IN_FALLBACK_REDIRECT_URL,
+      NEXT_PUBLIC_CLERK_SIGN_UP_FORCE_REDIRECT_UR:
+        process.env.NEXT_PUBLIC_CLERK_SIGN_UP_FORCE_REDIRECT_UR,
+      DATABASE_URL: process.env.DATABASE_URL,
+    };
+    const parsed = envSchema.parse(env);
+    logger.info("Environment variables validated successfully");
+    return parsed;
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      const missingVars = error.errors.map((err) => err.path.join("."));
+      logger.error("Invalid environment variables", { error: { missingVars } });
+      throw new Error(
+        `❌ Invalid environment variables: ${missingVars.join(
+          ", "
+        )}. Please check your .env file`
+      );
+    }
+    throw error;
+  }
+};
+
+export const env = validateEnv();
